8cee26573c82ca8c1ec28255bb41b4bfc6cefa9a
ddouglss <ddouglss1999@gmail.com>
2025-04-29 20:01:53 -0300

    Melhorando a função de Search e utilizar os dados no componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import {Outlet} from "react-router-dom";
+import module from './App.module.css';
 
 function App() {
 
   return (
-    <div className="App">
+    <div className={module.app}>
         <h1>Localize perfils do GitHub</h1>
         <Outlet/>
     </div>
